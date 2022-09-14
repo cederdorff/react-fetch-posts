8f7cca320298971079caebe87380e2b38dfec471
Rasmus Cederdorff <rasmus@cederdorff.com>
2022-09-14 08:24:18 +0200

    PostCard and fetch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import PostCard from "./components/PostCard";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        async function getPosts() {
+            const res = await fetch("https://raw.githubusercontent.com/cederdorff/mdu-frontend/main/data/posts.json");
+            const data = await res.json();
+            console.log(data);
+            setPosts(data);
+        }
+
+        getPosts();
+    }, []);
+
+    function createPost(event) {
+        event.preventDefault();
+        console.log(event.target);
+        const title = event.target.title.value;
+        const body = event.target.body.value;
+        const image = event.target.image.value;
+
+        const id = Date.now();
+        const newPost = {
+            title: title,
+            body: body,
+            image: image,
+            id: id
+        };
+
+        setPosts([newPost, ...posts]);
+    }
+
+    return (
+        <>
+            <header className="topbar">
+                <h1>React Post App</h1>
+            </header>
+            <main className="page">
+                <form onSubmit={createPost}>
+                    <input type="text" placeholder="Title" name="title" />
+                    <input type="text" placeholder="Body" name="body" />
+                    <input type="url" placeholder="Image url" name="image" />
+                    <button>Save</button>
+                </form>
+                <section className="grid-container">
+                    {posts.map(post => (
+                        <PostCard key={post.id} post={post} />
+                    ))}
+                </section>
+            </main>
+        </>
+    );
 }
 
 export default App;
---
+++ src/components/PostCard.js
@@ -0,0 +1,9 @@
+export default function PostCard({ post }) {
+    return (
+        <article>
+            <img src={post.image} alt={post.title} />
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+        </article>
+    );
+}
